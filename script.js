181da3f36b633dd7a7591f5d9ec1636d7a7c68b8
textwilly2-svg <textwilly2@gmail.com>
2026-07-19 00:43:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// placeholder
+function calculate() {
+
+let epi = parseFloat(document.getElementById("epi").value) || 0;
+let ppi = parseFloat(document.getElementById("ppi").value) || 0;
+let warp = parseFloat(document.getElementById("warp").value) || 1;
+let weft = parseFloat(document.getElementById("weft").value) || 1;
+
+let warpWt = epi / warp;
+let weftWt = ppi / weft;
+let gsm = (warpWt + weftWt) * 25;
+
+document.getElementById("warpWt").innerHTML = warpWt.toFixed(2);
+document.getElementById("weftWt").innerHTML = weftWt.toFixed(2);
+document.getElementById("gsm").innerHTML = gsm.toFixed(2);
+
+}
